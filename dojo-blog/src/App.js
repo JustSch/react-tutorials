22cf0ddba0bcf2f6bfe37a972e4eb872455b47ce
Justin Schreiber <jschreiber234@gmail.com>
2022-01-05 18:22:25 -0500

    styling / components

--- dojo-blog/src/App.js
+++ dojo-blog/src/App.js
@@ -1,16 +1,12 @@
-import './App.css';
+import Navbar from './Navbar';
+import Home from './Home';
 
 function App() {
-  const title = 'Welcome to The New Blog';
-  const likes = 50;
   return (
     <div className="App">
+      <Navbar />
       <div className="content">
-        <h1>{ title }</h1>
-        <p>Liked { likes } times</p>
-        <p>{ 25 }</p>
-        <p>{ [1,2,3,4,5] }</p>
-        <p>{ Math.random() * 10 }</p>
+        <Home />
       </div>
     </div>
   );
