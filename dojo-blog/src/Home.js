f9b323dac6f8be24357b3aebf6681859880537e9
Justin Schreiber <jschreiber234@gmail.com>
2022-01-05 18:36:50 -0500

    click events

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,7 +1,17 @@
 const Home = () => {
+    const handleClick = (e) => {
+        console.log('i\'m pomu', e);
+    }
+
+    const handleClickAgain = (name, e) =>{
+        console.log('hello '+ name,e.target);
+    }
+
     return ( 
         <div className="home">
             <h2>Homepage</h2>
+            <button onClick={handleClick}>Click Me</button>
+            <button onClick={(e) => handleClickAgain('Pomu',e)}>Click Me again</button>
         </div>
      );
 }
