dd63eec4e461833a29edc51b8a76cdc431efb43b
Justin Schreiber <jschreiber234@gmail.com>
2022-01-19 18:38:45 -0500

    conditional loading message

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -3,9 +3,7 @@ import BlogList from "./BlogList";
 
 const Home = () => {    
     const [blogs,setBlogs] = useState(null);
-
-  
-    
+    const [isPending,setIsPending] = useState(true);
 
     useEffect(() => {
         fetch('http://localhost:8000/blogs')
@@ -15,11 +13,13 @@ const Home = () => {
         .then((data) => {
             console.log(data);
             setBlogs(data);
+            setIsPending(false);
         })
     }, []);
 
     return ( 
         <div className="home">
+            {isPending && <div>Loading...</div>}
             {blogs && <BlogList blogs={blogs} title="All Blogs"/>}
         </div>
      );
