c11c8ac07a751815e03d151394533dabbd03fb55
Justin Schreiber <jschreiber234@gmail.com>
2022-01-18 20:11:37 -0500

    functions as props

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -7,10 +7,14 @@ const Home = () => {
         { title: 'Welcome party!', body: 'i\'m pomu', author: 'elira', id: 2 },
         { title: 'Web dev top tips', body: 'HAHAHAHAHAHAHA', author: 'selen', id: 3 }
     ]);
+
+    const handleDelete = (id) =>{
+        const newBlogs = blogs.filter(blog => blog.id !== id);
+        setBlogs(newBlogs);
+    }
     return ( 
         <div className="home">
-            <BlogList blogs={blogs} title="All Blogs" />
-            <BlogList blogs={blogs.filter((blog) => blog.author === 'elira')} title="Elira's Blogs" />
+            <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete}/>
         </div>
      );
 }
