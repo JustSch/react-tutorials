a60ea37e4b7652e02ac4417aa937a3811b115194
Justin Schreiber <jschreiber234@gmail.com>
2022-02-01 22:57:40 -0500

    programmic redirects

--- dojo-blog/src/Create.js
+++ dojo-blog/src/Create.js
@@ -1,10 +1,13 @@
 import { useState } from "react";
+import { useHistory } from 'react-router-dom';
 
 const Create = () => {
     const [title, setTitle] = useState('');
     const [body, setBody] = useState('');
     const [author, setAuthor] = useState('mario');
     const [isPending, setIsPending] = useState(false);
+    const history = useHistory();
+
     const handleSubmit = (e) => {
         e.preventDefault();
         const blog = {title, body, author};
@@ -15,7 +18,10 @@ const Create = () => {
             body: JSON.stringify(blog)
         }).then(()=>{
             setIsPending(false);
+            //history.go(-1);
+            history.push('/');
         });
+        
     }
     return ( 
         <div className="create">
